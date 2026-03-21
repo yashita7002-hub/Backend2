4664e58097d3e800c18dba5e1b3368a45644d18b
yashita7002@gmail.com <yashita7002@gmail.com>
2026-03-21 23:14:55 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import cookieParser from "cookie-parser "
+import cookieParser from "cookie-parser"
 
 
 const app = express()
@@ -17,9 +17,20 @@ app.use(
 
 
 app.use(express.json({limit: "16kb"}))//converts json into js
-
 app.use(express.urlencoded({extended: true, limit: "16kb"} ))//converts url data
 app.use(express.static("public"))//serves file that dont change on server that are images,etc
 app.use(cookieParser())//read cookies from the client request.
 
+
+
+
+
+
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/users", userRouter)
 export {app}
