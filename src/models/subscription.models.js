ca5850e46df4751e186e0cbd8cb753783225c4e4
yashita7002@gmail.com <yashita7002@gmail.com>
2026-03-22 20:38:29 +0530

    Subscription model

---
+++ src/models/subscription.models.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema({
+
+  subscriber:{
+    type: Schema.Types.ObjectId, //the one who is subscribing
+    ref:"User",
+  },
+
+  channel: {
+    type: Schema.Types.ObjectId,  //the one who is subscribing
+    ref: "User",
+  }
+
+
+
+
+},{timestamps:true})
+
+
+
+
+export const subscription = mongoose.model("Subscription", subscriptionSchema)
